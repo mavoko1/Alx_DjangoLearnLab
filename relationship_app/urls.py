b29fbaca5b34400d061ef4fb749735121bd0c67a
Eddy Mageza <magezaeddy@gmail.com>
2025-09-12 12:31:16 +0200

    Add book management app with home page, add/edit/delete functionality

--- relationship_app/urls.py
+++ relationship_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('add/', views.add_book, name='add_book'),
+    path('edit/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete/<int:pk>/', views.delete_book, name='delete_book'),
 ]
--- relationship_app/views.py
+++ relationship_app/views.py
@@ -1,14 +1,38 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
-from .models import Book, Library
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Book
+from .forms import BookForm
+from django.contrib.auth.decorators import permission_required
 
-# Function-based view to list all books
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+# Add Book
+@permission_required('relationship_app.add_book')
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = BookForm()
+    return render(request, 'relationship_app/add_book.html', {'form': form})
 
-# Class-based view to show library details
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
+# Edit Book
+@permission_required('relationship_app.change_book')
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'relationship_app/edit_book.html', {'form': form})
+
+# Delete Book
+@permission_required('relationship_app.delete_book')
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('home')
+    return render(request, 'relationship_app/delete_book.html', {'book': book})
